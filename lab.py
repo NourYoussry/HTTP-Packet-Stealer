03d38c34a3ab059448098a717091fdd78ce13e37
NourYoussry <eng-nour.yousry1621@alexu.edu.eg>
2020-04-08 18:12:25 +0200

    done parsing IP headers

--- lab.py
+++ lab.py
@@ -1,3 +1,7 @@
+import socket
+import binascii
+import operator
+import struct
 class IpPacket(object):
     """
     Represents the *required* data to be extracted from an IP packet.
@@ -35,11 +39,42 @@ def parse_application_layer_packet(ip_packet_payload: bytes) -> TcpPacket:
     # That's a byte literal (~byte array) check resources section
     return TcpPacket(-1, -1, -1, b'')
 
+def Int2IP(ipnum):
+    o1 = int(ipnum / 16777216) % 256
+    o2 = int(ipnum / 65536) % 256
+    o3 = int(ipnum / 256) % 256
+    o4 = int(ipnum) % 256
+    return '%(o1)s.%(o2)s.%(o3)s.%(o4)s' % locals()
+
+def to_string(ip):
+  "Convert 32-bit integer to dotted IPv4 address."
+  return ".".join(map(lambda n: str(ip>>n & 0xFF), [24,16,8,0]))
 
 def parse_network_layer_packet(ip_packet: bytes) -> IpPacket:
     # Parses raw bytes of an IPv4 packet
     # That's a byte literal (~byte array) check resources section
-    return IpPacket(-1, -1, "0.0.0.0", "0.0.0.0", b'')
+    # fields to parse : protocol, ihl, source_address, destination_address, payload
+    packet = ip_packet
+    IHL_mask = 0x0F
+    DEFAULT_HEADER_SIZE = 20 #bytes
+
+    #version(4-bits)#IHL(4-bits)#
+    version_IHL, = struct.unpack('!B', packet[:1])
+    IHL = version_IHL & IHL_mask
+    header_size = IHL * 4 #in Bytes
+
+    protocol, = struct.unpack('!B', packet[9:10])
+    print(protocol)
+
+    source_address, = struct.unpack('!I', packet[12:16])
+    source_address = to_string(source_address)
+
+    dest_address, = struct.unpack('!I', packet[16:DEFAULT_HEADER_SIZE])
+    dest_address = to_string(dest_address)
+
+    payload = packet[header_size:]
+    print((protocol, IHL, source_address, dest_address, payload))
+    return IpPacket(protocol, IHL, source_address, dest_address, payload)
 
 
 def main():
@@ -49,10 +84,13 @@ def main():
     # iface_name = "lo"
     # stealer.setsockopt(socket.SOL_SOCKET,
     #                    socket.SO_BINDTODEVICE, bytes(iface_name, "ASCII"))
-    while True:
-        # Receive packets and do processing here
-        pass
-    pass
+    #while True:
+
+        stealer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        raw_data =  stealer.recvfrom(4096)
+        #print(binascii.hexlify(raw_data[0]))
+        parse_network_layer_packet(raw_data)
+
 
 
 if __name__ == "__main__":
